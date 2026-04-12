5ee76a3ede41244a540afbb28c4b16b4f3a79fd9
nitin9990 <36775376+nitin9990@users.noreply.github.com>
2026-04-12 13:16:54 +0530

    Update app_intermediate.py

---
+++ app_intermediate.py
@@ -0,0 +1,9 @@
+from questions import INTERMEDIATE_TESTS
+from core import run_app
+
+run_app(
+    level      = "intermediate",
+    tests_map  = INTERMEDIATE_TESTS,
+    time_limit = 45 * 60,
+    title      = "Python Assessment — Intermediate (Banking / Data Analysis)",
+)
