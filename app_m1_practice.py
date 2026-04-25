f9de3f802d9598a7b4e896398827b2f7553779b0
nitin9990 <36775376+nitin9990@users.noreply.github.com>
2026-04-25 23:01:02 +0530

    Update app_m1_practice.py

--- app_m1_practice.py
+++ app_m1_practice.py
@@ -1,12 +1,12 @@
 import streamlit as st
-st.set_page_config(page_title="Module 1 Practice — Python Core", page_icon="🐍", layout="centered")
+
 try:
     from questions_m1 import M1_PRACTICE_TESTS
     from core import run_app
     run_app(
         level        = "m1_practice",
         tests_map    = M1_PRACTICE_TESTS,
-        time_limit   = 99999,   # no timer for practice
+        time_limit   = 99999,
         title        = "Module 1 Practice — Python Core",
         mode         = "practice",
         max_attempts = 3,
